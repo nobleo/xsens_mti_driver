74dedbe66e4f6eb0e937d5b0eeae31135101659d
Tim Clephas <tim.clephas@nobleo.nl>
2021-01-25 22:46:21 +0100

    Remove uncompilable and unused lines

--- lib/xspublic/xstypes/datapacket_p.h
+++ lib/xspublic/xstypes/datapacket_p.h
@@ -943,12 +943,12 @@ namespace XsDataPacket_Private {
 				for (int j = 0; j < 3; ++j)
 				{
 					{
-						auto& tmp = m_data.m_fingers[i].m_iV[j];
-						m_data.m_fingers[i].m_iV[j] = (uint32_t)swapEndian32(tmp);
+//						auto& tmp = m_data.m_fingers[i].m_iV[j];
+//						m_data.m_fingers[i].m_iV[j] = (uint32_t)swapEndian32(tmp);
 					}
 					{
-						auto& tmp = m_data.m_fingers[i].m_mag[j];
-						tmp = (int16_t)swapEndian16(tmp);
+//						auto& tmp = m_data.m_fingers[i].m_mag[j];
+//						tmp = (int16_t)swapEndian16(tmp);
 					}
 				}
 				{
